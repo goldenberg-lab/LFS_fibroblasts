15f159781b8c79ee68349a5395a3637e707d75de
Jaryd Hunter <jaryd.hunter@sickkids.ca>
2019-12-12 16:41:23 -0500

    added new config file for ga2m model, which separates test and train by different individuals.

--- scripts/ga2m.py
+++ scripts/ga2m.py
@@ -1,5 +1,6 @@
 import pickle as pk
 import pandas as pd
+import numpy as np
 from interpret.glassbox import ExplainableBoostingClassifier
 from random import seed
 from math import floor
@@ -44,17 +45,33 @@ res_dir = '/data/Jaryd/R/LFS_fibroblasts/ga2m/ga2m_results/' + \
 os.makedirs(res_dir)
 
 # Label mutant observations
-dat['mutant'] = dat.Row >= 3
-#dat['mutant'] = dat.Column >= 3
-# Drop labelling columns and shuffle data order.
-dat = dat.drop(columns=['Row', 'Column', 'Time', 'S', 'M', 'FocusScore3', 'FocusScore4', 'FocusScore5', 'Centroid_1', 'Centroid_2', 'Orientation']).sample(frac=1)
+dat['mutant'] = dat.Row > 3
+#dat['mutant'] = dat.Column >= (max(dat.Column) - min(dat.Column))/2 + min(dat.Column)
 
 # Apply given configuration
 if configuration['subset_features'][0] != 'None':
     dat = dat[configuration['subset_features'] + ['mutant']]
 
-dat_train = dat.iloc[:floor(len(dat)*0.9), :]
-dat_test = dat.iloc[floor(len(dat)*0.9):, :]
+# Drop labelling columns and shuffle data order.
+if configuration['test'][0] == 'random':
+    dat = dat.drop(columns=['Row', 'Column', 'Time', 'S', 'M', 'FocusScore3', 'FocusScore4', 'FocusScore5', 'Centroid_1', 'Centroid_2', 'Orientation']).sample(frac=1)
+
+    # Select random train and test sets.
+    dat_train = dat.iloc[:floor(len(dat) * 0.9), :]
+    dat_test = dat.iloc[floor(len(dat) * 0.9):, :]
+elif sum([b.isdigit() for b in configuration['test']]) == len(configuration['test']):
+    # Assume the values in configuration['test'] refer to rows which will only be in the test set.
+    if not (sum([int(b) in dat.Row for b in configuration['test']]) == len(configuration['test'])):
+        raise Exception('not all test values are rows in the data.')
+    test_rows = [int(r) for r in configuration['test']]
+    # Let the test set be a specific row which corresponds to one individual in our data.
+    dat_train = dat.loc[~dat['Row'].isin(test_rows)]
+    dat_train = dat_train.drop(columns=['Row', 'Column', 'Time', 'S', 'M', 'FocusScore3', 'FocusScore4', 'FocusScore5', 'Centroid_1', 'Centroid_2', 'Orientation'])
+    dat_test = dat.loc[dat['Row'].isin(test_rows)]
+    dat_test = dat_test.drop(columns=['Row', 'Column', 'Time', 'S', 'M', 'FocusScore3', 'FocusScore4', 'FocusScore5', 'Centroid_1', 'Centroid_2', 'Orientation'])
+else:
+    raise Exception('test = x, where x must be random, or a comma separated seq of digits which are valid rows in the '
+                    'data')
 
 ebm = ExplainableBoostingClassifier(interactions=int(configuration['num_interaction'][0]))
 ebm.fit(X=dat_train.drop(columns='mutant'), y=dat_train['mutant'])
